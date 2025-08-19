c966e6279c903816d20f767bdb02995f64cd84ac
Niilo Pyykkö <pyykkoniilo@gmail.com>
2025-08-19 17:22:19 +0300

    js beginning

---
+++ main.js
@@ -0,0 +1,10 @@
+// FAQ AKKORDION
+document.addEventListener("DOMContentLoaded", () => {
+  const faqContainer = document.querySelector(".faq-content");
+
+  faqContainer.addEventListener("click", (e) => {
+    const groupHeader = e.target.closest(".faq-group-header");
+
+    if (!groupHeader) return;
+  });
+});
